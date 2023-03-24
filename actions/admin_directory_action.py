3710aff6ee8a754077469b605e9f2d3ca8adf123
MurodjonT <murodjon.mmm.99@mail.ru>
2023-03-24 17:06:28 +0500

    directory news

--- actions/admin_directory_action.py
+++ actions/admin_directory_action.py
@@ -6,3 +6,7 @@ class DirectoryPageAction(HomePageNavigation):
     def open_directory_page(self, login1, password1):
         self.home_page_navigation(login1, password1)
         return True
+
+    def open_news_page(self, name, file):
+        self.check_add_news(name, file)
+        return True
--- tests/test_admin_directory.py
+++ tests/test_admin_directory.py
@@ -7,3 +7,4 @@ class TestDirectoryPage(TestBase):
     def test_directory_page(self):
         self.go_to_directory_page = DirectoryPageAction(self.driver)
         assert self.go_to_directory_page.open_directory_page("admin", "1234")
+        assert self.go_to_directory_page.open_news_page("test_news", "C:/zk/profskills/123456.jpg")
